8f886ed750fcfed4d80f012f09265c706a573465
amoon <amoon.salameh@almarx.com>
2025-01-16 23:51:17 +0000

    add no

--- app/controllers/health.controller.js
+++ app/controllers/health.controller.js
@@ -2,5 +2,5 @@ exports.healthCheck = (req, res) => {
   // Perform any logic needed for the health check, if necessary.
   // For now, we return a simple success status.
 
-  res.status(200).json({ status: "Healthy ok" });
+  res.status(200).json({ status: "Healthy ok Nooo" });
 };
